5838fa8dfffdcfda25e51185cd1dcb0cccf2c7e0
Daniil Cherkesov <daniilerkesov122@gmail.com>
2025-02-06 17:30:43 +0300

    create Loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,8 @@
+import { BarLoader } from "./BarLoader";
+export const Loader = () => {
+    return (
+        <div className="grid place-content-center bg-white px-4 py-24">
+            <BarLoader />
+        </div>
+    );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { App } from "./App.jsx";
+import { Suspense } from "react";
 import { BrowserRouter } from "react-router";
 
+import { App } from "./App.jsx";
+import { Loader } from "./components/Loader/Loader.jsx";
+
+import "./index.css";
+
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Suspense fallback={<Loader />}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Suspense>
 );
