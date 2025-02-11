a8c78bfcca657b1f42385ace034b5d1cc8a80dca
Daniil Cherkesov <daniilerkesov122@gmail.com>
2025-02-11 14:19:44 +0300

    fix cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: (origin, callback) => {
+            const allowedOrigins = [process.env.CLIENT_URL];
+            if (allowedOrigins.includes(origin) || !origin) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+    })
+);
 app.use(cookieParser());
 app.use("/api", router);
 app.use(errorMiddleware);
