faa78dfd535ae944120d0ccd9142882e23a01968
Daniil Cherkesov <daniilerkesov122@gmail.com>
2025-02-19 17:33:21 +0300

    create task-model

--- server/models/task-model.js
+++ server/models/task-model.js
@@ -1,13 +1,32 @@
 const mongoose = require("mongoose");
 
-const Task = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String },
-    dueDate: { type: Date },
-    status: { type: String, default: "Pending" },
-    priority: { type: String, default: "Normal" },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
+const TaskSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        description: { type: String },
+        dueDate: { type: Date },
+        status: {
+            type: String,
+            enum: ["Pending", "In Progress", "Completed"],
+            default: "Pending",
+        },
+        priority: {
+            type: String,
+            enum: ["Low", "Normal", "High"],
+            default: "Normal",
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        assignedTo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
 
-module.exports = mongoose.model("Task", Task);
+module.exports = mongoose.model("Task", TaskSchema);
