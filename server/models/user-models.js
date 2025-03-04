eaac4be6d413fc9d80e75b31643068d9fc135f5b
Daniil Cherkesov <daniilerkesov122@gmail.com>
2025-03-04 15:33:28 +0300

    removed the isActivated field in the user-model

--- server/models/user-models.js
+++ server/models/user-models.js
@@ -4,7 +4,6 @@ const UserSchema = new Schema(
     {
         email: { type: String, unique: true, required: true },
         password: { type: String, required: true },
-        isActivated: { type: Boolean, default: false },
         firstName: { type: String },
         lastName: { type: String },
         profilePhoto: { type: String },
