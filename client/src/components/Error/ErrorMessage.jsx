79637bcbf05ab7ba7b8669b40865c93d437ac233
Daniil Cherkesov <daniilerkesov122@gmail.com>
2025-03-05 19:54:16 +0300

    fix: errorMessage

--- client/src/components/Error/ErrorMessage.jsx
+++ client/src/components/Error/ErrorMessage.jsx
@@ -1,12 +1,12 @@
 import { motion } from "framer-motion";
 import { AlertCircle } from "lucide-react";
 
-export const ErrorMessage = ({
-    message = "An unexpected error occurred, please try again later.",
-}) => {
+export const ErrorMessage = ({ message }) => {
     const refreshPage = () => {
         window.location.reload();
     };
+
+    console.log(message);
 
     return (
         <motion.div
@@ -17,7 +17,9 @@ export const ErrorMessage = ({
             className="fixed top-[70px] right-4 bg-red-100 border-l-4 border-red-500 text-red-700 p-4 shadow-md rounded-lg flex items-center gap-3 max-w-md"
         >
             <AlertCircle className="w-6 h-6 text-red-500" />
-            <span className="text-md">{message}</span>
+            <span className="text-md">
+                {"An unexpected error occurred, please try again later."}
+            </span>
             <button
                 onClick={refreshPage}
                 className="ml-auto text-neutral-900 cursor-pointer hover:underline transition-colors"
