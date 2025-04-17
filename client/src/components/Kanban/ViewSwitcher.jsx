81427bf088ec50ee3f177b7ab23d4fdf1dad1156
Danya Cherkesov <daniilerkesov122@gmail.com>
2025-04-17 17:27:46 +0300

    del: Delete components

--- client/src/components/Kanban/ViewSwitcher.jsx
+++
@@ -1,8 +0,0 @@
-export const ViewSwitcher = ({ Icon, switchTitle, onClick }) => {
-    return (
-        <button className="flex gap-2 items-center justify-center p-2 cursor-pointer hover:bg-violet-300 dark:hover:bg-violet-500 transition-colors hover:rounded">
-            <Icon size={16} />
-            <span>{switchTitle}</span>
-        </button>
-    );
-};
