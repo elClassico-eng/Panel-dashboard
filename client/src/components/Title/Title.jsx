a1db53c17b97d1e0c02bde3c639113ac2b4a3543
Danya Cherkesov <daniilerkesov122@gmail.com>
2025-04-23 16:18:09 +0300

    Замена стилей в компоненте

--- client/src/components/Title/Title.jsx
+++ client/src/components/Title/Title.jsx
@@ -1,7 +1,7 @@
 export const Title = ({ title }) => {
     return (
         <>
-            <h1 className="dark:text-white text-gray-900 font-semibold text-3xl py-5 px-12">
+            <h1 className="dark:text-white text-gray-900 font-semibold text-3xl py-5 px-6">
                 {title}
             </h1>
         </>
